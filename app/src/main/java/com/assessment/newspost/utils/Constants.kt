deddead568934794a91e9f0994270d3aecb46c98
Muhammad Yoga Alifianto <muhammad.alifianto@students.amikom.ac.id>
2022-05-17 08:41:57 +0700

    change user agent

--- app/src/main/java/com/assessment/newspost/utils/Constants.kt
+++ app/src/main/java/com/assessment/newspost/utils/Constants.kt
@@ -3,7 +3,7 @@ package com.assessment.newspost.utils
 object Constants {
     interface CONFIG{
         companion object{
-            const val USER_AGENT = "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:37.0) Gecko/20100101 Firefox/37.0"
+            const val USER_AGENT = "Dalvic/2.1.0 (Linux; U; Android 10; Pixel 3 XL Build/QP1A.191005.007)"//"Mozilla/5.0 (Windows NT 6.3; WOW64; rv:37.0) Gecko/20100101 Firefox/37.0"
         }
     }
 }
